48cdbc701ec03828e33e42f57142c8b903f72ccd
Benjamin Eberlei <kontakt@beberlei.de>
2011-12-12 19:50:11 +0100

    DCOM-84 - Refactor proxy interface and Debugging, decoupled from ORM\Proxy

--- Debug.php
+++ Debug.php
@@ -1,7 +1,5 @@
 <?php
 /*
- *  $Id$
- *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -27,7 +25,6 @@ namespace Doctrine\Common\Util;
  * @license http://www.opensource.org/licenses/lgpl-license.php LGPL
  * @link    www.doctrine-project.org
  * @since   2.0
- * @version $Revision: 3938 $
  * @author  Guilherme Blanco <guilhermeblanco@hotmail.com>
  * @author  Jonathan Wage <jonwage@gmail.com>
  * @author  Roman Borschel <roman@code-factory.org>
@@ -53,80 +50,69 @@ final class Debug
     public static function dump($var, $maxDepth = 2, $stripTags = true)
     {
         ini_set('html_errors', 'On');
-        
+
         if (extension_loaded('xdebug')) {
             ini_set('xdebug.var_display_max_depth', $maxDepth);
         }
-        
+
         $var = self::export($var, $maxDepth++);
-        
+
         ob_start();
         var_dump($var);
         $dump = ob_get_contents();
         ob_end_clean();
-        
+
         echo ($stripTags ? strip_tags(html_entity_decode($dump)) : $dump);
-        
+
         ini_set('html_errors', 'Off');
     }
-    
+
     public static function export($var, $maxDepth)
     {
         $return = null;
         $isObj = is_object($var);
-    
+
         if ($isObj && in_array('Doctrine\Common\Collections\Collection', class_implements($var))) {
             $var = $var->toArray();
         }
-        
+
         if ($maxDepth) {
             if (is_array($var)) {
                 $return = array();
-            
+
                 foreach ($var as $k => $v) {
                     $return[$k] = self::export($v, $maxDepth - 1);
                 }
             } else if ($isObj) {
+                $return = new \stdclass();
                 if ($var instanceof \DateTime) {
-                    $return = $var->format('c');
+                    $return->__CLASS__ = "DateTime";
+                    $return->date = $var->format('c');
+                    $return->timezone = $var->getTimeZone()->getName();
                 } else {
-                    $reflClass = new \ReflectionClass(get_class($var));
-                    $return = new \stdclass();
-                    $return->{'__CLASS__'} = get_class($var);
+                    $reflClass = ClassUtils::newReflectionObject($var);
+                    $return->__CLASS__ = ClassUtils::getClass($var);
 
-                    if ($var instanceof \Doctrine\ORM\Proxy\Proxy && ! $var->__isInitialized__) {
-                        $reflProperty = $reflClass->getProperty('_identifier');
+                    if ($var instanceof \Doctrine\Common\Persistence\Proxy) {
+                        $return->__IS_PROXY__ = true;
+                        $return->__PROXY_INITIALIZED__ = $var->__isInitialized();
+                    }
+
+                    foreach ($reflClass->getProperties() as $reflProperty) {
+                        $name  = $reflProperty->getName();
+
                         $reflProperty->setAccessible(true);
-
-                        foreach ($reflProperty->getValue($var) as $name => $value) {
-                            $return->$name = self::export($value, $maxDepth - 1);
-                        }
-                    } else {
-                        $excludeProperties = array();
-
-                        if ($var instanceof \Doctrine\ORM\Proxy\Proxy) {
-                            $excludeProperties = array('_entityPersister', '__isInitialized__', '_identifier');
-                        }
-
-                        foreach ($reflClass->getProperties() as $reflProperty) {
-                            $name  = $reflProperty->getName();
-
-                            if ( ! in_array($name, $excludeProperties)) {
-                                $reflProperty->setAccessible(true);
-
-                                $return->$name = self::export($reflProperty->getValue($var), $maxDepth - 1);
-                            }
-                        }
+                        $return->$name = self::export($reflProperty->getValue($var), $maxDepth - 1);
                     }
                 }
             } else {
                 $return = $var;
             }
         } else {
-            $return = is_object($var) ? get_class($var) 
+            $return = is_object($var) ? get_class($var)
                 : (is_array($var) ? 'Array(' . count($var) . ')' : $var);
         }
-        
+
         return $return;
     }
 
