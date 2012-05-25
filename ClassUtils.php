43efd3f4c940f9f260f1642dddb53d916ffc90da
Benjamin Eberlei <kontakt@beberlei.de>
2012-05-25 23:48:59 +0200

    LGPL => MIT license

--- ClassUtils.php
+++ ClassUtils.php
@@ -13,7 +13,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * This software consists of voluntary contributions made by many individuals
- * and is licensed under the LGPL. For more information, see
+ * and is licensed under the MIT license. For more information, see
  * <http://www.doctrine-project.org>.
  */
 
