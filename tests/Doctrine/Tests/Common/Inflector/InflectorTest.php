76a186cbf73d8aa22a038fc892dd7a071a4f095a
Nauro da Silva Júnior <nauro@univali.br>
2017-09-18 11:50:19 -0300

    Fixing spaces

--- tests/Doctrine/Tests/Common/Inflector/InflectorTest.php
+++ tests/Doctrine/Tests/Common/Inflector/InflectorTest.php
@@ -74,7 +74,7 @@ class InflectorTest extends TestCase
             array('fungus', 'fungi'),
             array('glove', 'gloves'),
             array('gulf', 'gulfs'),
-			array('grave', 'graves'),
+            array('grave', 'graves'),
             array('half', 'halves'),
             array('hero', 'heroes'),
             array('hippopotamus', 'hippopotami'),
