7f6a1b435cdcd14cf1e24dabfb2951c69e04be2c
Noud de Brouwer <noud4@home.nl>
2020-12-01 23:45:05 +0100

    extra asked test words 3

--- tests/Doctrine/Tests/Inflector/Rules/Dutch/DutchFunctionalTest.php
+++ tests/Doctrine/Tests/Inflector/Rules/Dutch/DutchFunctionalTest.php
@@ -45,10 +45,12 @@ class DutchFunctionalTest extends LanguageFunctionalTest
             ['lof', 'lofbetuigingen'],
             ['quaestrix', 'quaestrices'],
             ['matrix', 'matrices'],
+            // @todo: newly check next 5 words
             ['meer', 'meren'],
             ['baas', 'bazen'],
             ['oog', 'ogen'],
             ['as', 'assen'],
+            ['kies', 'kiezen'],
             ['twitter', 'twitter'],
             // @todo: multiplitudes array w/o sort order for multiple plural same possibilities
             ['epos', 'epen'],
